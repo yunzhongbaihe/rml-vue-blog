f12e4ce568c454d75407378166d2f48b8e4223a2
吴小刚 <940374606@qq.com>
2020-06-05 09:07:03 +0800

    rml-vue-blog

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
+import AxiosApi from './api/axios';
 
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
+Vue.use(AxiosApi);
 
 /* eslint-disable no-new */
 new Vue({
